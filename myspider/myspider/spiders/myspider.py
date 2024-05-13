5ddf81113a950780cd0a9fbdc34300accbe040fa
boonecabaldev <164784238+boonecabaldev@users.noreply.github.com>
2024-05-13 13:30:40 +0000

    first working HatLeafSpider

--- myspider/myspider/spiders/myspider.py
+++ myspider/myspider/spiders/myspider.py
@@ -3,11 +3,42 @@ from myspider.items import MyspiderItem
 
 class MySpider(scrapy.Spider):
     name = 'myspider'
-    start_urls = ['http://example.com']
+    start_urls = ['https://newtrade6699.x.yupoo.com/albums']
 
     def parse(self, response):
+        links = response.xpath("//div[@class='showindex__children']//a[@class='album__main']/@href").getall()
+        for link in links:
+            yield scrapy.Request(response.urljoin(link), callback=self.parse_link)
+
+    def parse_link(self, response):
         for quote in response.css('div.quote'):
             item = MyspiderItem()
             item['title'] = quote.css('span.text::text').get()
             item['link'] = quote.css('span a::attr(href)').get()
             yield item
+
+#class HatLeafSpider(scrapy.Spider):
+#    name = 'HatLeafSpider'
+#    start_urls = ['https://newtrade6699.x.yupoo.com/albums/156268944?uid=1&isSubCate=false&referrercate=3515821']
+#
+#    def parse(self, response):
+#        for div in response.css('div.image__imagewrap'):
+#            yield {
+#                'data_src': div.css('img::attr(data-src)').get(),
+#                'data_origin_src': div.css('img::attr(data-origin-src)').get(),
+#                'data_path': div.css('img::attr(data-path)').get(),
+#                'src': div.css('img::attr(src)').get(),
+#            }
+
+class HatLeafSpider(scrapy.Spider):
+    name = 'HatLeafSpider'
+    start_urls = ['https://newtrade6699.x.yupoo.com/albums/156268944?uid=1&isSubCate=false&referrercate=3515821']
+
+    def parse(self, response):
+        for div in response.css('div.image__imagewrap'):
+            data_src = div.css('img::attr(data-src)').get()
+            data_origin_src = div.css('img::attr(data-origin-src)').get()
+            data_path = div.css('img::attr(data-path)').get()
+            src = div.css('img::attr(src)').get()
+
+            print(f"data_src: {data_src}, data_origin_src: {data_origin_src}, data_path: {data_path}, src: {src}")
---
+++ vim91_readme.md
@@ -0,0 +1,26 @@
+```sh
+sudo apt-get install -y git libncurses5-dev libgnome2-dev libgnomeui-dev \
+    libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
+    libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
+    python3-dev ruby-dev lua5.1 lua5.1-dev libperl-dev git
+
+sudo apt-get remove vim vim-runtime gvim
+
+git clone https://github.com/vim/vim.git
+
+cd vim
+
+./configure --with-features=huge \
+            --enable-multibyte \
+            --enable-rubyinterp=yes \
+            --enable-python3interp=yes \
+            --with-python3-config-dir=$(python3-config --configdir) \
+            --enable-perlinterp=yes \
+            --enable-luainterp=yes \
+            --enable-gui=gtk2 --enable-cscope --prefix=/usr
+make VIMRUNTIMEDIR=/usr/share/vim/vim82
+
+sudo make install
+
+vim --version
+```
